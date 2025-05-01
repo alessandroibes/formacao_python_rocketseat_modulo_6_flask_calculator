a3fd4f71b9f3941fa1e2a79fb70b840e2dd8c173
alessandroibes <alessandro.ibes@gmail.com>
2025-05-01 11:22:51 -0300

    Implementing the second calculator

---
+++ src/calculators/calculator_2.py
@@ -0,0 +1,46 @@
+from flask import Request as FlaskRequest
+from typing import Dict, List
+
+from src.drivers.numpy_handler import NumpyHandler
+
+
+class Calculator2:
+    '''
+    Segunda Calculadora
+
+    * N números são enviados.
+
+    * Todos esses N números são multiplicados por 11 e elevados 
+      a potência de 0.95.
+
+    * Por fim, é retirado o desvio padrão desses resultados e retornado 
+      o inverso desse valor (1/result).
+    '''
+
+    def calculate(self, request: FlaskRequest) -> Dict:
+        body = request.json
+        input_data = self.__validate_body(body)
+        calculated_number = self.__process_data(input_data)
+
+        return self.__format_response(calculated_number)
+
+    def __validate_body(self, body: Dict) -> List[float]:
+        if "numbers" not in body:
+            raise Exception("body mal formatado!")
+        
+        input_data = body["numbers"]
+        return input_data
+    
+    def __process_data(self, input_data: List[float]) -> float:
+        numpy_handler = NumpyHandler()
+        first_process_result = [(num * 11) ** 0.95 for num in input_data]
+        result = numpy_handler.standard_derivation(first_process_result)
+        return 1/result
+    
+    def __format_response(self, calculated_number: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 2,
+                "result": round(calculated_number, 2)
+            }
+        }
